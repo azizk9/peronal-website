568689070be049e4749054c940c674c62b7443d6
azizk9 <abdulazizkanjoagha@gmail.com>
2023-01-30 11:39:38 +0300

    added new wss

---
+++ templates/real-estate/js.js
@@ -0,0 +1,27 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if (hamburer) {
+  hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
+const swiper = new Swiper(".heroslider", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
