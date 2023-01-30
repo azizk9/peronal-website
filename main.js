a44e69983aa6cc0b558cf0c61f2767317ea4096e
azizK9 <abdulazizkanjoagha@gmail.com>
2023-01-30 11:08:11 +0300

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemeu.style.right = "0";
+  sidemeu.style.display = "block";
+}
+function closemenu() {
+  sidemeu.style.right = "-200px";
+  // sidemeu.style.display = "none";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxFPOumeDbJiqgWAk-0amckNNlNJFnw_qsVxtUCz0NdFsgIjAVks72krB6x-Q1HiuTTzw/exec";
+const form = document.querySelector(".contactForm");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
