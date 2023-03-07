c994464c3e2ec7aafa900cd4c1d490c620967119
azizk9 <abdulazizkanjoagha@gmail.com>
2023-03-07 20:49:12 +0300

    THE CHANGES MADE

--- templates/newproj/script.js
+++ templates/newproj/script.js
@@ -18,3 +18,37 @@ menu.onclick = function () {
     item.style.right = "-360px";
   }
 };
+
+const form = document.querySelector(".contactForm");
+const msgEl = document.getElementById("msg");
+
+async function handleSubmit(e) {
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbxFPOumeDbJiqgWAk-0amckNNlNJFnw_qsVxtUCz0NdFsgIjAVks72krB6x-Q1HiuTTzw/exec";
+
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+
+  e.preventDefault();
+
+  try {
+    await fetch(scriptURL, {
+      method: "POST",
+      body: {
+        name,
+        email,
+        message,
+      },
+    });
+
+    msgEl.innerHTML = "Message sent successfully!";
+    setTimeout(() => (msg.innerHTML = ""), 5000);
+    form.reset();
+  } catch (error) {
+    console.error("Error!", error.message);
+    msgEl.innerHTML = "Failed to send the message!";
+  }
+}
+
+form.addEventListener("submit", handleSubmit);
