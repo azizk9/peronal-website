fab508d601a155c0250e650a64fc2c99b0abb6f9
azizk9 <abdulazizkanjoagha@gmail.com>
2023-03-09 10:35:13 +0300

    THE CHANGES MADE

--- templates/newproj/script.js
+++ templates/newproj/script.js
@@ -52,3 +52,23 @@ async function handleSubmit(e) {
 }
 
 form.addEventListener("submit", handleSubmit);
+var swiper = new Swiper(".course-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
